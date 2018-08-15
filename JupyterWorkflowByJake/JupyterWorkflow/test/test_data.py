5c85607e3ba6ccb26ecff1beca6296093fe29a14
prashanth shanmuganathan <sprastar@dyn158133.cc.lehigh.edu>
2018-08-15 15:46:54 -0400

    Added make file

---
+++ JupyterWorkflowByJake/JupyterWorkflow/test/test_data.py
@@ -0,0 +1,6 @@
+from JupyterWorkflow.data import get_fremont_data
+import pandas as pd
+def test_fremont_data():
+    data = get_fremont_data()
+    assert(all(data.columns == ['West','East','Total']))
+    isinstance(data.index,pd.DatetimeIndex)
